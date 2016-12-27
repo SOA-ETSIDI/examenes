77a0a3b02d52ead401915180038d5ce1893ce487
BeatrizGomez <beatrizgbelver@gmail.com>
2016-12-27 18:00:44 +0100

    iu y server
    reutilizo del github de calendario ya que sirve para el uso de examenes

--- iu.R
+++ iu.R
@@ -1,3 +1,54 @@
 library(rhandsontable)
 library(shiny)
 library(shinyjs)
+
+source('init.R')
+addResourcePath("pdf", tempdir())
+
+
+logoUPM <- "http://www.upm.es/sfs/Rectorado/Gabinete%20del%20Rector/Logos/UPM/EscPolitecnica/EscUpmPolit_p.gif"
+logoETSIDI <- "http://www.upm.es/sfs/Rectorado/Gabinete%20del%20Rector/Logos/EUIT_Industrial/ETSI%20DISEN%C2%A6%C3%A2O%20INDUSTRIAL%20pqn%C2%A6%C3%A2.png"
+
+
+## Cabecera con logos
+header <- fluidRow(
+  column(4, align = 'center', img(src = logoUPM)),
+  column(4, align = 'center',
+         h2("Calendario Académico"),
+         h5("Subdirección de Ordenación Académica")),
+  column(4, align = 'center', img(src = logoETSIDI))
+)
+
+editor <- div(id = 'editor',
+              fluidRow(
+                column(12, 
+                       rHandsontableOutput('table'))
+              ),
+              br(),
+              fluidRow(
+                column(12,
+                       actionButton("update",
+                                    "Confirmar",
+                                    icon = icon("check"))
+                )
+              ))
+
+pdfUI <- fluidRow(column(12,
+                         htmlOutput("pdfViewer")
+))
+
+## UI completa
+shinyUI(
+  fluidPage(
+    useShinyjs(),  # Set up shinyjs
+    includeCSS("styles.css"),
+    header,
+    fluidRow(
+      column(12,
+             tabsetPanel(
+               tabPanel('Tabla', editor),
+               tabPanel('Vista', pdfUI)
+             ))
+    )
+  ))
+
--- server.R
+++ server.R
@@ -1,3 +1,56 @@
 library(rhandsontable)
 library(shiny)
 library(shinyjs)
+
+source('init.R')
+addResourcePath("pdf", tempdir())
+
+cal <- leeCalendario()
+makeCalPDF(cal)
+
+shinyServer(function(input,output,session){
+  disable("update")
+  
+  output$table <- renderRHandsontable({
+    cal[, Inicio := as.character(Inicio)]
+    cal[, Final := as.character(Final)]
+    
+    hot <- rhandsontable(cal,
+                         rowHeaders = NULL,
+                         language = 'es')
+    ## Las columnas no se editan ni ordenan
+    hot <- hot_cols(hot,
+                    allowColEdit = FALSE,
+                    columnSorting = FALSE)
+    hot
+  })
+  
+  output$pdfViewer <- renderUI(
+    {
+      ## Añado enlace reactivo para que actualice contenido del
+      ## iframe si actualizo tabla
+      refresh <- input$table
+      tags$iframe(style="height:600px; width:100%",
+                  src=paste0("pdf/ETSIDI_", cursoActual, 
+                             ".pdf#zoom=page-width"))
+    })
+  ## Refresco PDF
+  observeEvent(input$table,
+               {
+                 enable("update")
+                 df <- hot_to_r(input$table)
+                 try(makeCalPDF(df))
+               })
+  
+  ## Grabo datos en csv
+  observeEvent(input$update,
+               {
+                 df <- hot_to_r(input$table)
+                 write.csv(df,
+                           file = paste0('calendarioETSIDI_', cursoActual, '.csv'),
+                           row.names = FALSE)
+                 info('Tabla modificada correctamente.')
+               })
+  
+}) 
+
