8fb24d8bd9ded74d3ef46fe9ab106af205a1065f
BeatrizGomez <beatrizgbelver@gmail.com>
2017-02-19 19:10:48 +0100

    extraigo y empiezo a comparar

--- init.R
+++ init.R
@@ -22,3 +22,38 @@ asigAC <- subset(asignaturas,Titulacion=='56AC')
 
 cal_examen <- read.csv2("../examenes/examenes finales.csv")
 #analizo lo que esta puesto y lo comparo
+
+##Tambien se puede suponer que examenes finales es un excel
+# instalo paquete que lea excell
+# install.packages("xlsx")
+
+# Verifico si esta bien instalado
+#any(grepl("xlsx",installed.packages()))
+# cargo la libreria xlsx
+#library("xlsx")
+#library("rJava")
+
+
+#exam <- read.xlsx("../examenes/examenes finales.xlsx", sheetIndex = 1)
+#print(data)
+
+#Tengo que extraer las columnas de cada grado
+
+attach(cal_examen)
+attach(exam)
+search()
+
+#ingeniería electrica
+#ingenieria electrica industrial y automatica
+#ingeniería mécanica
+#ingeniería química
+#ingeniería diseño industrial y desarrollo del producto
+
+examIE <- ingeniería electrica
+examIQ <- ingeniería química
+examIA <- ingenieria electrica industrial y automatica
+examIM <- ingeniería mécanica
+examDD <- ingeniería diseño industrial y desarrollo del producto
+#examDM <- subset(exam,Titulacion=='56DM')
+#examEE <- subset(exam,Titulacion=='56EE')
+
